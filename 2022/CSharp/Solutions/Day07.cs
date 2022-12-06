f08f80301835419524d5904082443497d15a9ab8
Teis Lindemark <teis.lindemark@fhi.com>
2022-12-06 13:30:42 +0100

    Prep day07

---
+++ 2022/CSharp/Solutions/Day07.cs
@@ -0,0 +1,19 @@
+public sealed class Day07 : Solver
+{
+    public Day07(string inputPath) : base(inputPath) {}
+
+    protected override Task Setup()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override async Task<object> Part1()
+    {
+        return await Task.FromResult("");
+    }
+
+    protected override async Task<object> Part2()
+    {
+        return await Task.FromResult("");
+    }
+}
